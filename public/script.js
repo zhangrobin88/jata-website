a03dc5b8348703c7889f7ae9f3c4e1e4c307219f
zhangrobin88 <zhangrobin.contact@gmail.com>
2024-08-20 20:54:30 +0200

    Joe & The Anchor Website
    Saving the project to github so that I can  make changes to it easier in the future.

--- public/script.js
+++ public/script.js
@@ -1,34 +1,3 @@
-// When enter is pressed in password field we will check if the password i correct.
-// If correct we will be directed to the Hidden page.
-// This codesnippet will load lastly.
-const correctPassword = "jataForever";
-let loggedIn = false;
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.getElementById("ip2");
-    
-    input.addEventListener("keydown", function(event) {
-      if (event.key === "Enter" && window.location.pathname !== "/hiddenPage.html") {
-        if (input.value === correctPassword) {
-            loggedIn = true;
-            if (loggedIn) {
-                window.location.href = "hiddenPage.html";
-                document.getElementById("hiddenPage").display = "none";
-                console.log("Logged in");
-            }
-            
-        } else {
-            loggedIn = false;
-            console.log("Enter pressed passwrod incorrect!")
-            console.log(input.value);
-            input.value = "";
-            document.getElementById("hiddenPage").display = "none";
-            console.log("Not logged in");
-        }
-      }
-    });
-  });
 
   function smoothScroll(){
     document.querySelector('.tourDatesParentContainer').scrollIntoView({
@@ -91,7 +60,7 @@ function newBtn(elementId, imgSrc) {
     document.getElementById(elementId).src=imgSrc;
 }
 
-
+/*
 function overlayLoginPromptOn() {
     document.getElementById("overlay").style.display = "flex";
     document.getElementById("overlay").style.justifyContent="center";
@@ -101,7 +70,7 @@ function overlayLoginPromptOn() {
   function overlayLoginPromptOff() {
     document.getElementById("overlay").style.display = "none";
   }
-
+*/
 
 
 
